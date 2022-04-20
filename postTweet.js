5da5c82cf0e3654cb16ca4c1128c22c6447b56f8
David Quintero <davidq@gmail.com>
2022-04-19 22:01:11 -0400

    handle rejected promise

--- postTweet.js
+++ postTweet.js
@@ -3,12 +3,18 @@ const { Post } = require('./models/post');
 
 async function postTweet() {
   setInterval(async function () {
-    const data = await Post.find({ published: false });
+    let data;
+    try {
+      data = await Post.find({ published: false });
+      console.log('data', data);
+    } catch (ex) {
+      console.log(ex);
+    }
 
     if (data[0]) {
-      console.log(data[0]); //current tweet
-      await twitterClient.tweets.statusesUpdate({ status: data[0].title + ' #LessWrong ' + data[0].url });
+      console.log('next to tweet', data[0]); //current tweet
       try {
+        await twitterClient.tweets.statusesUpdate({ status: data[0].title + ' ' + data[0].url });
         data[0].published = true;
         await data[0].save();
       } catch (ex) {
