953304b742125e58baf1d24df2c57b26b16f8136
David Quintero <davidq@gmail.com>
2022-04-19 22:14:45 -0400

    Set timer to top of the hour

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const lessWrongScraper = require('./scraper');
 const postTweet = require('./postTweet');
 require('./db')();
 
+// 0 Interval: 1 hour
+
+const interval = async () => {
+  const mins = new Date().getMinutes();
+  console.log(mins, mins == 0);
+
+  if (mins == 0) {
+    console.log('SCRAPING FOR NEW POSTS');
+    await lesswrongScraper();
+  }
+};
+
 // 1 - Scrape content to json file
-// Interval: 1 hour
 
 let firstRun = false;
 
@@ -16,9 +27,9 @@ async function init() {
 
 init();
 
-setInterval(async function () {
-  await lessWrongScraper();
-}, 3600000);
+setInterval(() => {
+  interval();
+}, 1000);
 
 // 2 - Post tweet at 1 minute interval
 
