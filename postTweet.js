3263ce387b877f81beea115670ec3162d45f640a
David Quintero <davidq@gmail.com>
2022-08-13 22:29:46 -0400

    Crop tweet string to less than 240 characters

--- postTweet.js
+++ postTweet.js
@@ -1,6 +1,15 @@
 const twitterClient = require('./twitterClient');
 const { Post } = require('./models/post');
 
+const cropTitleTo240Characters = (str) => {
+  let tweet = str.split('');
+  if (tweet.length > 220) {
+    const array = tweet.splice(0, 220);
+    return array.join('') + ' [...]';
+  }
+  return str;
+};
+
 async function postTweet() {
   setInterval(async function () {
     let data;
@@ -13,11 +22,13 @@ async function postTweet() {
     if (data[0]) {
       console.log('next to tweet', data[0]); //current tweet
       try {
-        await twitterClient.tweets.statusesUpdate({ status: data[0].title + ' ' + data[0].url });
+        await twitterClient.tweets.statusesUpdate({
+          status: cropTitleTo240Characters(data[0].title) + ' ' + data[0].url,
+        });
         data[0].published = true;
         await data[0].save();
       } catch (ex) {
-        console.log('error?', ex.message);
+        console.log('error log:', ex);
       }
     }
   }, 60000);
