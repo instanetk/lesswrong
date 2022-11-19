a583f11e8e9f2ce458980ccd6d2ce1a4df727e26
David Quintero <davidq@gmail.com>
2022-11-18 22:36:52 -0500

    Add environment variable for GPT-3

---
+++ gpt3.js
@@ -0,0 +1,23 @@
+const { Configuration, OpenAIApi } = require('openai');
+require('dotenv').config();
+
+const configuration = new Configuration({
+  organization: 'org-tU9uFf5jkFxLFNlZM5AngwEC',
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const getGPT3SummaryDescription = async (url) => {
+  const response = await openai.createCompletion({
+    model: 'text-davinci-002',
+    prompt: 'Summarize this post ' + url,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  return response.data;
+};
+
+module.exports = getGPT3SummaryDescription;
