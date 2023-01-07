345f39ffa3ff181de20da22d63a5e08102803155
David Quintero <davidq@gmail.com>
2023-01-07 12:55:09 -0500

    lower temperature

--- gpt3.js
+++ gpt3.js
@@ -11,7 +11,7 @@ const getGPT3SummaryDescription = async (url) => {
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: 'Summarize this: ' + url,
-    temperature: 0.7,
+    temperature: 0,
     max_tokens: 256,
     top_p: 1,
     frequency_penalty: 0,
